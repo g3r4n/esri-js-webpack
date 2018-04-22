12d7c2aa3d34d963775d680514e2dedfd39e19a8
Enguerrand des Vaux <edesvaux@esriaustralia.com.au>
2018-04-22 13:36:45 +1000

    upgrade package

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+const title = "My Minimal React Webpack Babel Setup";
+
+const App = () => <div>{title}</div>;
+
+export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = "My Minimal React Webpack Babel Setup";
 
-ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
-);
-
-module.hot.accept();
+ReactDOM.render(<App />, document.getElementById("app"));
