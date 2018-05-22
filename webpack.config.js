3f864a2432c3efd3233c871792d833a2ed3446bb
Enguerrand des Vaux <edesvaux@esriaustralia.com.au>
2018-05-22 21:46:34 +1000

    Fix assets issue

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,25 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|webp)$/,
+        loader: "url-loader",
+        options: {
+          // Inline files smaller than 10 kB (10240 bytes)
+          limit: 10 * 1024,
+        }
+      },
+      {
+        test: /\.(wsv|ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
@@ -31,7 +50,10 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new ArcGISPlugin(),
+    new ArcGISPlugin({
+      // disable provided asset loaders
+      useDefaultAssetLoaders: false
+    }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "./index.html",
